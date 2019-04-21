bee1834e04e54c0887655866f6d67cce5c57567b
Wahid Rahim <wahidrahim@gmail.com>
2019-04-21 00:56:07 -0400

    removed unused code

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,32 +1,15 @@
 <template lang="pug">
   #game
-    //- Container width is calculated before mount.
     board
-    // .container(
-    //   :style='{\
-    //     width: `${containerWidth}px`,\
-    //     height: `${containerWidth}px`\
-    //   }')
-      
-    //   //- Cells are rendered "linearly". They wrap to the
-    //       next row based on their width (calculated in Cell.vue)
-    //   template(v-for='x in size')
-    //     template(v-for='y in size')
-    //       cell(:row='x', :col='y',
-    //         :size='size',
-    //         :containerWidth='containerWidth',
-    //         :key='`r${x}c${y}`',
-    //         :ref='`r${x}c${y}`',
-    //         @changeColor='changeColor')
 
-      //- TODO: make separate component
-      .actions(v-if='gameOver')
-        .action(@click="")
-          i.mdi.mdi-sword-cross
-          |  Send Challenge
-        .action(@click="showSaveScoreModal = true")
-          i.mdi.mdi-content-save-outline
-          |  Save
+    //- TODO: make separate component
+    .actions(v-if='gameOver')
+      .action(@click="")
+        i.mdi.mdi-sword-cross
+        |  Send Challenge
+      .action(@click="showSaveScoreModal = true")
+        i.mdi.mdi-content-save-outline
+        |  Save
 
     colors(@changeColor='changeColor')
 
@@ -45,7 +28,6 @@ import SaveScoreModal from '@/components/SaveScoreModal.vue'
 
 @Component({
   components: {
-    // Cell,
     Colors,
     SaveScoreModal,
     Board
@@ -65,75 +47,6 @@ export default class Home extends Vue {
   private showSaveScoreModal = false
   private containerWidth = 0
 
-  /**
-   * Set the neighbours of each cell
-   */
-  // private mounted() {
-  //   for (let r = 1; r <= this.size; r++) {
-  //     for (let c = 1; c <= this.size; c++) {
-  //       const cell = this.getCellComponent(r, c)
-
-  //       cell.setNeighbors([
-  //         this.getCellComponent(r, c - 1), // left
-  //         this.getCellComponent(r - 1, c), // top
-  //         this.getCellComponent(r, c + 1), // right
-  //         this.getCellComponent(r + 1, c) // bottom
-  //       ])
-
-  //       // save the numeric representation of the initilized board
-  //       this.board.push(cell.color)
-  //     }
-  //   }
-  // }
-
-  public changeColor(color: number) {
-    const firstCell = this.getCellComponent(1, 1)
-
-    if (color !== firstCell.color && !this.gameOver) {
-      firstCell.changeColor(color)
-      this.moves++
-
-      // check if game is over
-      const colors: number[] = []
-
-      iterator: for (let r = 1; r <= this.size; r++) {
-        for (let c = 1; c <= this.size; c++) {
-          if (colors.length > 1) {
-            break iterator // break out asap
-          }
-          if (!colors.includes(this.getCellComponent(r, c).color)) {
-            colors.push(color)
-          }
-        }
-      }
-
-      if (colors.length === 1) {
-        this.gameOver = true
-      }
-    }
-  }
-
-  /**
-   * Set the width of the board
-   */
-  private beforeMount() {
-    const MAX_WIDTH = 480
-    const PADDING = 32
-
-    this.containerWidth =
-      document.body.clientWidth < MAX_WIDTH
-        ? document.body.clientWidth - PADDING
-        : MAX_WIDTH - PADDING
-  }
-
-  /**
-   * Helper method to return the Cell vue component
-   */
-  private getCellComponent(row: number, col: number) {
-    const cell = this.$refs[`r${row}c${col}`] as any[]
-
-    return cell ? cell[0] : null
-  }
 }
 </script>
 
