f5dabcfe451772544a3fed4c77c40e9e017681d3
Wahid Rahim <wahidrahim@gmail.com>
2019-04-23 22:17:41 -0400

    wip - color buttons

--- src/components/Colors.vue
+++ src/components/Colors.vue
@@ -1,18 +1,30 @@
 <template lang="pug">
   ul.colors
-    li.color-0(@click="$emit('changeColor', 0)")
-    li.color-1(@click="$emit('changeColor', 1)")
-    li.color-2(@click="$emit('changeColor', 2)")
-    li.color-3(@click="$emit('changeColor', 3)")
-    li.color-4(@click="$emit('changeColor', 4)")
-    li.color-5(@click="$emit('changeColor', 5)")
+    li(v-for='(color, i) in colors' :key='i'
+    :class='`color-${i}`'
+    :style='{ backgroundColor: color }')
+
+    // li.color-0(@click="$emit('changeColor', 0)")
+    // li.color-1(@click="$emit('changeColor', 1)")
+    // li.color-2(@click="$emit('changeColor', 2)")
+    // li.color-3(@click="$emit('changeColor', 3)")
+    // li.color-4(@click="$emit('changeColor', 4)")
+    // li.color-5(@click="$emit('changeColor', 5)")
 </template>
 
 <script>
 import { Vue, Component } from 'vue-property-decorator'
 
 @Component
-export default class Colors extends Vue {}
+export default class Colors extends Vue {
+  get colors() {
+    return this.$store.getters.colors
+  }
+
+  created() {
+    console.log(this.colors);
+  }
+}
 </script>
 
 <style lang="sass" scoped>
@@ -33,21 +45,21 @@ $color-5: #a300ff
     width: calc((100% - 32px) / 6)
     height: 4rem
 
-    &:first-child
-      border-radius: 8px 0 0 8px
-    &:last-child
-      border-radius: 0 8px 8px 0
+    // &:first-child
+    //   border-radius: 8px 0 0 8px
+    // &:last-child
+    //   border-radius: 0 8px 8px 0
 
-    &.color-0
-      background-color: $color-0
-    &.color-1
-      background-color: $color-1
-    &.color-2
-      background-color: $color-2
-    &.color-3
-      background-color: $color-3
-    &.color-4
-      background-color: $color-4
-    &.color-5
-      background-color: $color-5
+    // &.color-0
+    //   background-color: $color-0
+    // &.color-1
+    //   background-color: $color-1
+    // &.color-2
+    //   background-color: $color-2
+    // &.color-3
+    //   background-color: $color-3
+    // &.color-4
+    //   background-color: $color-4
+    // &.color-5
+    //   background-color: $color-5
 </style>
